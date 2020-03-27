4d2b8624034c863214bdcebb3d51198096ec5d7d
Alexandra Tanner <alexandratanner@Alexandras-Air.localdomain>
2020-03-27 14:06:18 -0400

    deleted extra methods

--- index.js
+++ index.js
@@ -44,24 +44,6 @@ MongoClient.connect(url, function(err, db) {
   });
 });
 
-
-// This is the definition of the Person class
-class Person {
-    constructor(username, password, id, name, phone, email, birthdate, height, weight, hair, eyes) {
-        this.username = username;
-        this.password = password;
-        this.id = id;
-        this.name = name;
-        this.phone = phone;
-        this.email = email;
-        this.birthdate = birthdate;
-        this.height = height;
-        this.weight = weight;
-        this.hair = hair;
-        this.eyes = eyes;
-    }
-}
-
 //renders homepage
 app.use('/home', (req, res) => {
   res.render('main.ejs', {req: req, message: null});
@@ -72,57 +54,9 @@ app.use('/signup', (req, res) => {
   res.render('signup.ejs', {req: req, message: null});
 });
 
-// This is the '/test' endpoint that you can use to check that this works
-app.use('/test', (req, res) => {
-    // create a JSON object
-    var data = { 'message' : 'It works!' };
-    // send it back
-    res.json(data);
-});
-
-app.use('/get', (req, res) => {
-    var id = req.query.id;
-    var matches = [];
-
-    if (Array.isArray(id)) {
-      id.forEach((i) => {
-        var found = false;
-        people.forEach((person) => {
-          if (person.id === i) {
-            matches.push(person);
-            found = true;
-          }
-        });
-        if (!found) {
-          matches.push(new Person(i, 'unknown', new Date().getTime()));
-        }
-      });
-    } else {
-      people.forEach((person) => {
-        if (person.id === id) {
-          matches.push(person);
-        }
-      });
-    }
-
-    if (id) {
-      res.send(matches);
-    } else {
-      res.send([]);
-    }
-});
-
-// This endpoint allows a caller to add data to the Map of Person objects
-app.use('/set', (req, res) => {
-    // read id and status from query parameters
-    var id = req.query.id;
-    var status = req.query.status;
-    // create new Person object
-    var person = new Person(id, status, new Date().getTime());
-    // add it to Map
-    people.set(id, person);
-    // send it back to caller
-    res.json(person);
+//checks if username is taken, if not signs user up
+app.use('/checksignup', (req, res) => {
+  res.render('signup.ejs', {req: req, message: null});
 });
 
 // This starts the web server on port 3000.
