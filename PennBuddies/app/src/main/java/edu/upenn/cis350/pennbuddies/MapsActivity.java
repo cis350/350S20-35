8399a97fab33ca10990f8442d72904c7d879ddb6
Divya Somayajula <divyasomayajula@Divyas-MBP.lan>
2020-04-06 15:05:22 -0400

    map zooms into campus and marks buildings

--- PennBuddies/app/src/main/java/edu/upenn/cis350/pennbuddies/MapsActivity.java
+++ PennBuddies/app/src/main/java/edu/upenn/cis350/pennbuddies/MapsActivity.java
@@ -40,8 +40,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Penn and move the camera
-        LatLng penn = new LatLng(39, -75);
+        LatLng penn = new LatLng(39.9509461, -75.1935127);
         mMap.addMarker(new MarkerOptions().position(penn).title("University of Pennsylvania"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(penn));
+
+
+        //Add markers for College Houses
+        LatLng harnwell = new LatLng(39.9524, -75.2002);
+        LatLng hill = new LatLng(39.9530, -75.1907);
+        LatLng radian = new LatLng(39.9524, -75.2012);
+        LatLng quad = new LatLng(39.950806, -75.197306);
+
+        //Add markers for Academic buildings
+        LatLng drl = new LatLng(39.9519, -75.1900);
+        LatLng towne = new LatLng(39.9517, -75.1912);
+        LatLng vanpelt = new LatLng(39.9527, -75.1940);
+        LatLng huntsman = new LatLng(39.9522, -75.1983);
+
+        //Add markers for other important buildings
+        LatLng houston = new LatLng(39.9509, -75.1939);
+        LatLng hospital = new LatLng(39.9501, -75.1939);
+
+        mMap.addMarker(new MarkerOptions().position(harnwell).title("Harnwell College House"));
+        mMap.addMarker(new MarkerOptions().position(hill).title("Hill College House"));
+        mMap.addMarker(new MarkerOptions().position(radian).title("The Radian"));
+        mMap.addMarker(new MarkerOptions().position(quad).title("The Quadrangle"));
+        mMap.addMarker(new MarkerOptions().position(drl).title("David Rittenhouse Labaratory"));
+        mMap.addMarker(new MarkerOptions().position(towne).title("Towne Building"));
+        mMap.addMarker(new MarkerOptions().position(vanpelt).title("Van Pelt Library"));
+        mMap.addMarker(new MarkerOptions().position(huntsman).title("Huntsman"));
+        mMap.addMarker(new MarkerOptions().position(houston).title("Houston Hall"));
+        mMap.addMarker(new MarkerOptions().position(hospital).title("Hospital of UPenn"));
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(penn, 15f));
+
+
+
+
+
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(penn));
     }
 }
